63a0d9fdcfd46c339b18196f084a6a3732ab7a6f
Francisco Araújo <franciscocoelhoaraujo492000@gmail.com>
2024-12-13 17:40:07 +0000

    feat: 04_primitive_types and quiz1 exercises completed

--- solutions/quizzes/quiz1.rs
+++ solutions/quizzes/quiz1.rs
@@ -1,27 +1,14 @@
-// This is a quiz for the following sections:
-// - Variables
-// - Functions
-// - If
-//
 // Mary is buying apples. The price of an apple is calculated as follows:
 // - An apple costs 2 rustbucks.
 // - However, if Mary buys more than 40 apples, the price of each apple in the
 // entire order is reduced to only 1 rustbuck!
 
-// TODO: Write a function that calculates the price of an order of apples given
-// the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
-
-fn calculate_price_of_apples(apples: u64) -> u64 {
-    let mut price = 2;
-    
-    if apples > 40 {
-        price = 1;
-        apples * price
+fn calculate_price_of_apples(n_apples: u64) -> u64 {
+    if n_apples > 40 {
+        n_apples
     } else {
-        apples * price
+        2 * n_apples
     }
-
 }
 
 fn main() {
